2538f55ecc1a1dc166305fe32e32e7377aaa221f
Bishoy Sargius <63624116+bsargius@users.noreply.github.com>
2021-08-03 23:59:53 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+package arithmatic;
+
+public class Main {
+
+	public static void main(String[] args) { //Main
+		int num1 = 1;
+		int num2 = 2;
+		
+		System.out.println("Lets go Bois!\n");
+		printNew();
+		
+	}
+	
+	static void printNew() { //the keyword static makes it so that printNew is not associated with class Main
+		System.out.println("This is not part of arithmatic class?");
+	}
+	
+	static void add() {
+		
+	}
+
+}
